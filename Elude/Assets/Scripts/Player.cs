b80549a63dfeb7a5b4e868b644b496e4a0a66267
Isaac Williams <williamsisaac1209@gmail.com>
2020-10-08 11:38:32 +1100

    Added death to player
    beginnings of target bounce mushroom is also underway

--- Elude/Assets/Scripts/Player.cs
+++ Elude/Assets/Scripts/Player.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
@@ -9,8 +7,10 @@ public class Player : MonoBehaviour
     bool canJump = true;
     // Is the player currently climbing
     bool isClimbing = false;
-    // Is the player just used a bounce pad
-    bool bounce = false;
+    // Is the player using a bounce pad
+    bool isBouncing = false;
+    // Is the player alive
+    bool isAlive = true;
     // How high the player can jump
     public float jumpHeight;
     // How fast the player can climb
@@ -21,27 +21,37 @@ public class Player : MonoBehaviour
     Vector3 playerVelocity;
     // The direction the player is moving in
     Vector3 moveDirection;
+    // The starting position of the player
+    Vector3 startPos;
     // This one is pretty self explanitory
     CharacterController controller;
     // Instance of MakeGoBoing script
     MakeGoBoing makeGoBoing;
-
+    TargetedBounce targetedBounce;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        transform.position = startPos;
     }
 
     // Update is called once per frame
     // 
     void Update()
     {
+        if (!isAlive)
+        {
+            //Do death
+            gameObject.SetActive(false);
+            transform.position = startPos;
+            gameObject.SetActive(true);
+        }
         if (isClimbing || controller.isGrounded)
         { 
             canJump = true;
-            bounce = false;
+            isBouncing = false;
         }
-        if (canJump && !bounce)
+        if (canJump && !isBouncing)
         {
             playerVelocity.y = 0.0f;            
         }
@@ -98,10 +108,17 @@ public class Player : MonoBehaviour
         }
         else if (other.gameObject.tag == "Bouncy")
         {
-            bounce = true;
+            isBouncing = true;
             makeGoBoing = other.GetComponent<MakeGoBoing>();
             playerVelocity.y += Mathf.Sqrt(makeGoBoing.boingHeight * -3.0f * Physics.gravity.y);
-            Debug.Log("Boing");
+        }
+        else if (other.gameObject.tag == "Targeted Bounce")
+        {
+             targetedBounce = other.GetComponent<TargetedBounce>();
+            playerVelocity.x += targetedBounce.Target.x;
+            playerVelocity.y += Mathf.Sqrt(targetedBounce.Target.y * -3.0f * Physics.gravity.y);
+            playerVelocity.z += targetedBounce.Target.z;
+
         }
     }
 
---
+++ Elude/Assets/Scripts/TargetedBounce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetedBounce : MonoBehaviour
+{
+    public Vector3 Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, 0.1f / transform.parent.localScale.y, transform.localScale.z);
+        transform.localPosition = new Vector3(0, (0.1f / transform.parent.localScale.y) + transform.parent.localScale.y / 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
