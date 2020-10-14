e5e543502426239141b61749defc992833475560
Tom Collyer <karzacinc@gmail.com>
2020-10-14 16:23:34 +1100

    Added Game Manager
    Will be modified to have scene transitions when the base of menuing is complete

---
+++ Elude/Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public bool toggleTimer = false;
+
+    public float timer;
+    private float internalTimer;
+
+    public Player player;
+
+    private bool isGamePaused;
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        internalTimer = timer;
+        isGamePaused = false;
+    }
+
+    void Update()
+    {
+
+        #region Timer (Scrapped)
+
+        if (toggleTimer)
+        {
+            internalTimer -= Time.deltaTime;
+        }
+        if (internalTimer <= 0f)
+        {
+            toggleTimer = false;
+            internalTimer = timer;
+            GameOver();
+        }
+
+        #endregion
+
+        #region PauseMenu
+
+        if (Input.GetKeyDown("escape"))
+        {
+            if (isGamePaused == false)
+            {
+                isGamePaused = true;
+                Time.timeScale = 0.0f;
+
+                pauseMenu.SetActive(true);
+            }
+            else if (isGamePaused == true)
+            {
+                isGamePaused = false;
+                Time.timeScale = 1.0f;
+
+                pauseMenu.SetActive(false);
+
+            }
+        }
+
+        #endregion
+
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("The game is over"); // << Temporary
+
+        //Needs scene transition to game over scene
+    }
+
+
+}
