ee1eb149cd879ef86b49228005fa9bdebf32ba7c
Tom Collyer <karzacinc@gmail.com>
2020-10-14 16:44:50 +1100

    Added Flytrap, Prefab & Screen Fade (Missing Art)
    The flytrap script, along with a basic prefab has been added to the project, the script is contained within the trigger in the prefab, and requires a reference to the game manager within the scene, as well as the fade panel in the GUI, the player, and the output location.
    
    Will be updated when art assets are available.

---
+++ Elude/Assets/Scripts/Flytrap.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flytrap : MonoBehaviour
+{
+    public float snapTimer = 5;
+    private float timer;
+    private float fadeTimer;
+
+    private bool isTriggered;
+    private bool isInMouth;
+    private bool isPlayerCaught = false;
+
+    private bool runFlytrapSequence = false;
+
+    public GameManager gameManager;
+
+    public GameObject outputLocation;
+    public GameObject player;
+
+    public Image fadePanel; 
+
+    private void Start()
+    {
+        timer = snapTimer;
+        fadeTimer = 0;
+        fadePanel.color = Color.black;
+        fadePanel.CrossFadeAlpha(0, 1f, false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isInMouth = true;
+
+            if (isTriggered == false)
+            {
+                timer = snapTimer;
+
+                isTriggered = true;
+            }
+
+        }
+    }
+    void Update()
+    {
+
+        if (timer >= 0 && isTriggered == true)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0 && isInMouth == true && isTriggered == true)
+        {
+            PlayerCaught();
+        }
+
+        if (timer <= 0 && isPlayerCaught == false )
+        {
+            isTriggered = false;
+        }
+
+
+        if (runFlytrapSequence == true)
+        {
+            //Hides the player
+            player.GetComponent<MeshRenderer>().enabled = false;
+
+            if (fadeTimer <= 0)
+            {
+
+                // Moves player to output location
+                player.transform.position = outputLocation.transform.position;
+                fadePanel.CrossFadeAlpha(0, 1f, false);
+
+                //Shows the player
+                player.GetComponent<MeshRenderer>().enabled = true;
+
+                //Resets the variables
+                runFlytrapSequence = false;
+                isPlayerCaught = false;
+
+            }
+        }
+
+        //Timer for the screen fade
+        fadeTimer -= Time.deltaTime;
+
+    }
+
+    /// <summary>
+    /// Lets the flytrap know when the player has left its mouth
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isInMouth = false;
+        }  
+    }
+
+    /// <summary>
+    /// Is called when the player is successfully caught in the mouth of the flytrap
+    /// </summary>
+    private void PlayerCaught()
+    {
+        // Fades screen to black
+        fadeTimer = 1;
+        fadePanel.CrossFadeAlpha(1, 1f, false);
+
+        //Reset flytrap
+        isPlayerCaught = true;
+        isTriggered = false;
+        runFlytrapSequence = true;
+    }
+}
