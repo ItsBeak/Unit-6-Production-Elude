54b87fc470ef8ab83ba69f1732360ccdc71211b3
Tom Collyer <karzacinc@gmail.com>
2020-10-14 16:27:33 +1100

    Added Custom Trigger Tool
    This is a QoL tool for use of the designers if they feel its necessary. It can:
     - Enable Objects
     - Disable Objects
     - Spawn Objects
     - Remove itself upon completion
    
    More features may come later if needed

---
+++ Elude/Assets/Scripts/TriggerTool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TriggerTool : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool enable;
+
+    public enum TriggerType {enableObject, disableObject, instantiateObject}
+    public TriggerType onTrigger;
+
+    public enum OnCompletion { nothing, disableTrigger }
+    public OnCompletion onCompletion;
+
+    [Header("Objects - NOT ALL ARE NEEDED")]
+    public GameObject targetObject;
+    public GameObject destination;
+    public GameObject instantiatedObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enable)
+        {
+            if (onTrigger == TriggerType.enableObject)
+            {
+                targetObject.gameObject.SetActive(true);
+            }
+            else if (onTrigger == TriggerType.disableObject)
+            {
+                targetObject.gameObject.SetActive(false);
+            }
+            else if (onTrigger == TriggerType.instantiateObject)
+            {
+                Instantiate(instantiatedObject, destination.transform);
+            }
+
+            if (onCompletion == OnCompletion.nothing)
+            {
+                // do nothing ( ͡° ͜ʖ ͡°)
+            }
+            else if (onCompletion == OnCompletion.disableTrigger)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
