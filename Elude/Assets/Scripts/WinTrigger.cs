fd8bf9bc26c624ce671912c2eea2e06ae2ec7037
Tom Collyer <karzacinc@gmail.com>
2020-11-12 12:14:07 +1100

    Pursuer Working In New Level
     - Trap Plants Timer Adjusted To 1.5 seconds

---
+++ Elude/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+
+    public SceneSwitcher switcher;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            switcher.ToggleSceneChange();
+        }
+    }
+
+}
